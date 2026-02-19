fd56382af8681d494a619b5a3a17767539e1a79e
Souza <lucas11souza97@gmail.com>
2026-02-18 22:22:57 -0400

    feat: added validation to accuracy

--- app/07_fisherface_model.py
+++ app/07_fisherface_model.py
@@ -1,7 +1,7 @@
 
 import cv2
 import matplotlib.pyplot as plt
-from cv2.typing import MatLike
+
 import cv2.face
 from os import listdir
 from os.path import isfile, join
@@ -13,25 +13,23 @@ filepath_faces_treino = "base/imagens/treino/"
 filepath_faces_teste = "base/imagens/teste/"
 
 
-def padronizar_imagens(imagem_camimho: str) -> MatLike:
-  image: MatLike = cv2.imread(imagem_camimho, cv2.IMREAD_GRAYSCALE)
-  image: MatLike  = cv2.resize(image, (200, 200))
+def padronizar_imagens(imagem_camimho: str):
+  image = cv2.imread(imagem_camimho, cv2.IMREAD_GRAYSCALE)
+  image = cv2.resize(image, (200, 200))
   return image
 
 list_faces_treino = [f for f in listdir(filepath_faces_treino) if isfile(join(filepath_faces_treino, f))] 
 list_faces_teste = [f for f in listdir(filepath_faces_teste) if isfile(join(filepath_faces_teste, f))] 
 
-def generete_dados(list_faces: list[str], filepath_face: str) -> tuple[list[MatLike], list[str]]:
-  dados_treinamento: list[MatLike] = []
-  sujeitos_id: list[str] = []
-
-  for arq in list_faces:
+def generete_dados(list_faces: list[str], filepath_face: str):
+  dados_treinamento, sujeitos = [], []
+  for i, arq in enumerate(list_faces):
     image_path = filepath_face + arq
-    image: MatLike = padronizar_imagens(image_path)
+    image = padronizar_imagens(image_path)
     dados_treinamento.append(image)
-    sujeito_id = arq[1:3]
-    sujeitos_id.append(int(sujeito_id))
-  return dados_treinamento, sujeitos_id
+    sujeito = arq[1:3]
+    sujeitos.append(int(sujeito))
+  return dados_treinamento, sujeitos
 
 
 print()
@@ -52,29 +50,29 @@ sujeitos = np.asarray(sujeitos_treino, dtype=np.int32)
 sujeitos_teste = np.asarray(sujeitos_teste, dtype=np.int32)
 
 
-modelo_LBPH = cv2.face.LBPHFaceRecognizer.create()
-modelo_LBPH.train(dados_treinamento, sujeitos)
+modelo_fisherfaces = cv2.face.FisherFaceRecognizer.create()
+modelo_fisherfaces.train(dados_treinamento, sujeitos)
 
 
 # plt.figure(figsize=(20, 10))
 
 # plt.subplot(121)
-# plt.title("Sujeito " + str(sujeitos_teste[21]))
-# plt.imshow(dados_teste[21], cmap="gray")
+# plt.title("Sujeito " + str(sujeitos_teste[13]))
+# plt.imshow(dados_teste[13], cmap="gray")
 
 # plt.subplot(122)
-# plt.title("Sujeito " + str(sujeitos_teste[27]))
-# plt.imshow(dados_teste[27], cmap="gray")
+# plt.title("Sujeito " + str(sujeitos_teste[19]))
+# plt.imshow(dados_teste[19], cmap="gray")
 
 # plt.show()
 
-result = modelo_LBPH.predict(dados_teste[21])
+result = modelo_fisherfaces.predict(dados_teste[13])
 print(result)
 
 y_pred = []
 
 for dado in dados_teste:
-  y_pred.append(modelo_LBPH.predict(dado)[0])
+  y_pred.append(modelo_fisherfaces.predict(dado)[0])
 
 accuracy = accuracy_score(sujeitos_teste, y_pred)
 print('accuracy: ')
